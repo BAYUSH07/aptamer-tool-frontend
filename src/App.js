371070044acfc641325a2f49e76110fc2e954e45
Ayush Bhoj <mitu22btbi0021@students.mituniversity.edu.in>
2025-08-14 16:28:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css';
 import './App.css';
 import { ToastContainer, toast } from 'react-toastify';
