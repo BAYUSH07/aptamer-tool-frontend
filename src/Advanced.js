c2fd9fd6b6d7206337027fa18439d5647187a8b4
Ayush Bhoj <mitu22btbi0021@students.mituniversity.edu.in>
2025-08-14 16:27:51 +0530

    Update Advanced.js

--- src/Advanced.js
+++ src/Advanced.js
@@ -1,6 +1,6 @@
 // Advanced.js
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
